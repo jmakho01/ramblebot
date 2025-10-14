12d449a4bf4193e6503764945e8622078e81583e
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2025-10-14 12:24:00 -0700

    Added testTokenizeWithManySpaces test in LowercaseSentenceTokenizerTest.java

--- src/LowercaseSentenceTokenizerTest.java
+++ src/LowercaseSentenceTokenizerTest.java
@@ -19,8 +19,15 @@ class LowercaseSentenceTokenizerTest {
     /*
      * Write your test here!
      */
-    
+    @Test
+    void testTokenizeWithManySpaces() {
+        LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
+        Scanner scanner = new Scanner("hello     hi hi hi    hello hello");
+        List<String> tokens = tokenizer.tokenize(scanner);
 
+        assertEquals(List.of("hello", "hi", "hi", "hi", "hello", "hello"), tokens);
+    }
+    
     // Wave 3
     @Test
     void testTokenizeWithCapitalization() {
